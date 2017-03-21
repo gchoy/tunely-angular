77d10def3d3cf00e6e7dc4ff04dba7ba2ce3d696
gchoy <gabriela.choy@gmail.com>
2017-03-21 15:33:55 -0700

    added GET POST DELETE methods

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,27 +7,49 @@
   .module('tunely',[])
   .controller('AlbumsIndexController', AlbumsIndexController);
 
-  function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+function AlbumsIndexController ($http) {
     var vm = this;
     vm.newAlbum = {};
+    vm.newAlbum = {
+    name: 'Viva Hate',
+    artistName: 'Morrissey'
+    };
+    vm.albums =[];
 
-    vm.albums = [
-      {
-        name: 'License to Ill',
-        artistName: 'Beastie Boys'
-      },
-    {
-    name: 'Coming Home',
-    artistName: 'Leon Bridges'
-    },
-    {
-    name: 'Are We There',
-    artistName: 'Sharon Van Etten'
-    },
-    {
-    name: 'The Queen is Dead',
-    artistName: 'The Smiths'
-    }
-    ];
+
+$http({
+ method: 'GET',
+ url: '/api/albums'
+}).then(function successCallback(response) {
+  console.log(response);
+  vm.albums = response.data;
+}, function errorCallback(response) {
+ console.log('There was an error getting the data', response);
+});
+
+vm.createAlbum = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums',
+      data: vm.newAlbum,
+    }).then(function successCallback(response) {
+      vm.albums.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+}
+vm.deleteAlbum = function(album){
+  $http({
+    method: 'DELETE',
+    url: '/api/albums/' + album._id
+  }).then(function successCallback(json) {
+    var index = vm.albums.indexOf(album);
+    console.log("index is:" + index);
+    vm.albums.splice(index,1)
+  }, function errorCallback(reponse) {
+    console.log('There was an error deleting')
+});
+}
 
 }
